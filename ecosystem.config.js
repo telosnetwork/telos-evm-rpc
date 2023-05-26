65d2dea2c2d38d01b786f838f715bc7a95566b4b
Jesse Schulman <jesse@caleos.io>
2023-05-26 12:38:37 -0700

    Use cluster mode in pm2 for multiple instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ const config = require('./config.json');
 module.exports = {
   apps : [{
     name: config.pm2Name,
+    exec_mode: "cluster",
     instances: config.pm2Instances,
     script: 'dist/main.js',
   }],
