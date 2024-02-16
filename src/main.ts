8cf359bff2be2d47ee22096cc8918adb38d8bc53
Guillermo Rodriguez <guillermo@telos.net>
2024-02-16 00:02:54 -0300

    Add cmdline option for config.json path

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import TelosEVMRPC from './TelosEVMRPC'
 import {TelosEvmConfig} from "./types";
 import {EventEmitter} from 'events';
+import {Command} from 'commander';
+import {readFileSync} from 'fs';
 
 EventEmitter.defaultMaxListeners = 400;
 
-const config: TelosEvmConfig = require("../config.json") as TelosEvmConfig;
-const rpc: TelosEVMRPC = new TelosEVMRPC(config);
+const program = new Command();
 
-;(async () => {
-    console.log("Starting Telos EVM RPC...");
-    await rpc.start();
-    console.log("Telos EVM RPC started!!!");
-})()
+program
+    .option('-c, --config [path to config.json]', 'Path to config.json file', 'config.json')
+    .action(async (options) => {
+        const config: TelosEvmConfig = JSON.parse(readFileSync(options.config).toString());
+        const rpc: TelosEVMRPC = new TelosEVMRPC(config);
+
+        console.log("Starting Telos EVM RPC...");
+        await rpc.start();
+        console.log("Telos EVM RPC started!!!");
+    });
+
+program.parse(process.argv);
