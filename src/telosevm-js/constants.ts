b754965b59d582e800d609e2ba44372eca9a245e
tomtomcrypto <titiomm@gmail.com>
2024-06-13 15:54:27 +0000

    switched fork to london

--- src/telosevm-js/constants.ts
+++ src/telosevm-js/constants.ts
@@ -1,5 +1,5 @@
 export const ETH_CHAIN = 'mainnet'
-export const FORK = 'istanbul'
+export const FORK = 'london'
 export const DEFAULT_GAS_LIMIT = '0x1E8480'
 export const DEFAULT_VALUE = '0x0'
 export const DEFAULT_CHAIN_ID = 1
