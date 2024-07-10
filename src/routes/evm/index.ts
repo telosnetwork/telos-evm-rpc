e1ac2775e2baecf171e9e79dc7174defbef2c6bb
tomtomcrypto <titiomm@gmail.com>
2024-07-10 00:45:21 +0000

    remove toHex cast

--- src/routes/evm/index.ts
+++ src/routes/evm/index.ts
@@ -1230,8 +1230,8 @@ export default async function (fastify: FastifyInstance, opts: TelosEvmConfig) {
 			if(receipt['max_fee_per_gas']){
 				// Calculation for effectiveGasPrice is block.baseFeePerGas + min(trx.maxFeePerGas - block.baseFeePerGas, trx.maxPriorityFeePerGas).
 				const minProtocolBaseFee = new BN(MIN_PROTOCOL_BASE_FEE);
-				const maxFeePerGas = new BN(toHex(receipt['max_fee_per_gas']));
-				const maxPriorityFeePerGas = new BN(toHex(receipt['max_priority_fee_per_gas']));
+				const maxFeePerGas = new BN(receipt['max_fee_per_gas']);
+				const maxPriorityFeePerGas = new BN(receipt['max_priority_fee_per_gas']);
 				console.log(minProtocolBaseFee.toString(), maxFeePerGas.toString(), maxPriorityFeePerGas.toString());
 				const effectiveGasPrice = minProtocolBaseFee.add(minBN([maxFeePerGas.sub(minProtocolBaseFee), maxPriorityFeePerGas])).toString(16);
 				console.log(effectiveGasPrice.toString());
